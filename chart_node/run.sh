6ac52b7375a23fb78321313dd863629f6fcd8c13
peter <peter@aw.ro>
2019-07-05 21:20:59 +0300

    Working with STDIN

--- chart_node/run.sh
+++ chart_node/run.sh
@@ -1,4 +1,17 @@
-echo Hello World
+#!/bin/bash
+set -e
 
-exec node /config/testchart.js
+echo "Ready for input..."
 
+# Read input from STDIN
+while read input; do
+    echo "Read $input"
+    if ! msg="$(node /config/${input//\"}.js)"; then
+    	echo "[Error] Chart failed $msg"
+    fi
+	echo "Ready for next..."
+done
+
+echo "Exiting..."
+
+# Usage: {"addon":"local_chart_node", "input":"testchart"}
